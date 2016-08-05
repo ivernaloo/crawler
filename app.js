660011b599fb7d2e47291582ab41b12d098e5301
aloo <aloocs@gmail.com>
2016-08-05 18:06:19 +0800

    完成了数据写入以及更新

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 
 var pageToVisit = "http://www.0daydown.com/category/tutorials/other";
-var Store_Lcloud = 'https://api.leancloud.cn/1.1/classes/ScrapeResource';
+var Store_Lcloud = 'https://api.leancloud.cn/1.1/classes/Post';
+var Store_Lcloud_ScrapeResource = 'https://api.leancloud.cn/1.1/classes/ScrapeResource';
+var Store_Lcloud_Base = 'https://api.leancloud.cn/1.1/';
+var Store_Lcloud_Batch = "https://api.leancloud.cn/1.1/batch";
 var Lcloud = {
     'x-avoscloud-application-id': "lvwj1mpo0ikouhkwl956kwqbnegzj9y5nh6ybs4qx2vmyc4z",
     'x-avoscloud-application-key': "fhkv9jj22qsvmfmhtkj84mxzn5oytuw8fpb9vkywz9docpet"
 };
+var QueryString = {
+    limit : 50,
+    order : "-updatedAt"
+};
 
-request(pageToVisit, function(error, response, body) {
+getAll();
+
+
+/*request(pageToVisit, function(error, response, body) {
     if(error) {
         console.log("Error: " + error);
     }
@@ -21,41 +31,51 @@ request(pageToVisit, function(error, response, body) {
         var $ = cheerio.load(body);
         var data = {};
         var _list = [];
-        $('.excerpt header a').each(function(i, v){
+        // console.log("body : ", body);
+        $('.excerpt header h2 a').each(function(i, v){
             var _url = $(v).attr("href"),
-                _name = _url.split(/\/|\./).slice(-3,-1).join("");
-            data[_name] = {
-                "name" : $(v).text(),
-                "url" : _url
-            }
+                _name = $(v).text();
+            _list.push({
+                "method": "POST",
+                "path": "/1.1/classes/ScrapeResource",
+                "body": {
+                    "name": _name,
+                    "url" : _url
+                }
+            });
         });
-        console.log(data)
-        // record(data)
-        // getAll();
+        // console.log("data : ", _list);
+        record({
+            "requests" : _list
+        });
     }
-});
+})*/;
 
 function record(data){
-    request({
+    request.post({
         headers: Lcloud,
-        uri: Store_Lcloud,
-        json: true,
+        url: Store_Lcloud_Batch,
         body: data,
-        method: "POST"
+        json: true
     },function(err,res,body){
-        // console.log(err)
+        // console.log(err,body);
+        // console.log(res.statusCode.toString())
         // console.log(res)
        // console.log("Err : ",err,"\nRes : ",res,"\nBody : ", body)
     });
 }
 
 function getAll(){
+    console.log(".....");
     request({
         headers: Lcloud,
-        uri: Store_Lcloud,
-        method: "GET"
+        url: Store_Lcloud_ScrapeResource,
+        method: "GET",
+        qs: QueryString
     },function(err,res,body){
-        console.log("getAll Status code: " + res.statusCode);
+        console.log(JSON.parse(res.body).results);
+        // console.log(JSON.parse(body).result);
+        // console.log("getAll Status code: " + res.statusCode);
         // console.log("Err : ",err,"\nRes : ",res,"\nBody : ", body)
     });
 }
