6d68234a0ab9b18c1279eea8ff9a31687db237b7
aloo <aloocs@gmail.com>
2016-08-16 18:35:23 +0800

    加入了定时器

--- crawler/app.js
+++ crawler/app.js
@@ -3,7 +3,9 @@ var cheerio = require('cheerio');
 var cloud = require('../data/leancloud');
 var URL = require('url-parse');
 
-var TargetURL = "http://www.0daydown.com/category/tutorials/other";
+var URL = ["http://www.0daydown.com/category/tutorials/other"
+    , "http://www.0daydown.com/category/tutorials/移动app开发"
+    , "http://www.0daydown.com/category/tutorials/web-design"]
 
 var _B_Exist_List = [],
     _A_Update_List = [],
@@ -18,42 +20,68 @@ cloud.getAll().then(
     }
 );
 
+@todo 做一个遍历抓取
+var COUNT = 1;
+setTimeout(function(){
+    console.log("---", COUNT);
+    COUNT++
+}, 100)
+/*
 
-// request the target url
-request(TargetURL, function(error, response, body) {
-    if(error) {
-        console.log("Error: " + error);
+var ST = setTimeout(function(){
+    console.log("COUNT : ", COUNT);
+    console.log("URL : ", URL[COUNT-1] + "/page/" + COUNT);
+    if (COUNT < 4) {
+        console.log("Request");
+        crawler(URL[COUNT-1] + "/page/" + COUNT);
+        ++COUNT;
+    } else {
+        console.log("Clear");
+        clearTimeout(ST);
     }
-    // Check status code (200 is HTTP OK)
-    console.log("Status code: " + response.statusCode);
-    if(response.statusCode === 200) {
-        // Parse the document body
-        var $ = cheerio.load(body);
-        var data = {};
-        var _list = [];
-        // console.log("body : ", body);
-        $('.excerpt header h2 a').each(function(i, v){
-            var _url = $(v).attr("href"),
-                _name = $(v).text();
-            _list.push({
-                "method": "POST",
-                "path": "/1.1/classes/crawler",
-                "body": {
-                    "name": _name,
-                    "url" : _url
-                }
+
+},500);*/
+
+
+function crawler(url){
+    // request the target url
+    request(url, function(error, response, body) {
+        if(error) {
+            console.log("Error: " + error);
+        }
+        // Check status code (200 is HTTP OK)
+        console.log("Status code: " + response.statusCode);
+        if(response.statusCode === 200) {
+            // Parse the document body
+            var $ = cheerio.load(body);
+            var data = {};
+            var _list = [];
+            // console.log("body : ", body);
+            $('.excerpt header h2 a').each(function(i, v){
+                var _url = $(v).attr("href"),
+                    _name = $(v).text();
+                _list.push({
+                    "method": "POST",
+                    "path": "/1.1/classes/crawler",
+                    "body": {
+                        "name": _name,
+                        "url" : _url
+                    }
+                });
             });
-        });
-        // console.log("A data : ", _list);
-        _A_Update_List = _list;
-        _Update_list = diffArray(_A_Update_List, _B_Exist_List);
-        console.log(" Update LIST ", _Update_list, " Number : ", _Update_list.length);
+            // console.log("A data : ", _list);
+            _A_Update_List = _list;
+            _Update_list = diffArray(_A_Update_List, _B_Exist_List);
+            console.log(" Update LIST ", _Update_list, " Number : ", _Update_list.length);
 
-        cloud.record({
-            "requests" : _Update_list
-        });
-    }
-});
+            cloud.record({
+                "requests" : _Update_list
+            });
+        }
+    });
+}
+
+
 
 
 /*
