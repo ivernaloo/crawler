f6cfc3467f61d501d3e11bcf56f7023f8b19cfd1
aloo <aloocs@gmail.com>
2016-08-16 16:26:30 +0800

    加入了定时器

--- data/cron.js
+++ data/cron.js
@@ -1,9 +1,13 @@
 var cronJob = require('cron').CronJob;
+var path = require('path');
+var config = require('../config');
+var spawn = require('child_process').spawn;
 
 
 // 定时执行更新任务
 module.exports.job = new cronJob(config.autoUpdate, function () {
     console.log('开始执行定时更新任务');
+    console.log(process.execPath);
     var update = spawn(process.execPath, [path.resolve(__dirname, '../crawler/app.js')]);
     update.stdout.pipe(process.stdout);
     update.stderr.pipe(process.stderr);
