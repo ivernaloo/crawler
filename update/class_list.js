a882c261005e0f127e50f2b68e4ec5270b47104c
aloo <aloocs@gmail.com>
2016-08-17 22:56:29 +0800

    加入了支持翻页的逻辑

--- update/class_list.js
+++ update/class_list.js
@@ -2,30 +2,56 @@ var request = require('request');
 var cheerio = require('cheerio');
 var debug = require('debug')('blog:update');
 
-debug('读取博文类别列表');
+debug('读取博文列表');
 
-// 读取博客首页
-request('http://blog.sina.com.cn/u/1776757314', function (err, res) {
-    if (err) return console.error(err);
+/*
+* callback 处理错误的回调函数
+* */
+function readArticleList (url, callback) {
+    // 读取分类页面
+    request(url, function (err, res) {
+        if (err) return callback(err);
 
-    // 根据网页内容创建DOM操作对象
-    var $ = cheerio.load(res.body.toString()); // 抓取整个页面下来
+        // 根据网页内容创建DOM操作对象
+        var $ = cheerio.load(res.body.toString());
 
-    // 读取博文类别列表
-    var classList = [];
-    $('.classList li a').each(function () {
-        var $me = $(this);
-        var item = {
-            name: $me.text().trim(),
-            url:  $me.attr('href')
-        };
-        // 从URL中取出分类的ID
-        var s = item.url.match(/articlelist_\d+_(\d+)_\d\.html/);
-        if (Array.isArray(s)) {
-            item.id = s[1];
-            classList.push(item);
+        // 读取博文列表
+        var articleList = [];
+        $('.articleList .articleCell').each(function () {
+            var $me = $(this);
+            var $title = $me.find('.atc_title a');
+            var $time = $me.find('.atc_tm');
+            var item = {
+                title: $title.text().trim(),
+                url:   $title.attr('href'),
+                time:  $time.text().trim()
+            };
+            // 从URL中取出文章的ID
+            var s = item.url.match(/blog_([a-zA-Z0-9]+)\.html/);
+            if (Array.isArray(s)) {
+                item.id = s[1];
+                articleList.push(item);
+            }
+        });
+
+        // 检查是否有下一页
+        var nextUrl = $('.SG_pgnext a').attr('href');
+        if (nextUrl) {
+            // 读取下一页
+            readArticleList(nextUrl, function (err, articleList2) {
+                if (err) return callback(err);
+
+                // 合并结果
+                callback(null, articleList.concat(articleList2));
+            });
+        } else {
+            // 返回结果
+            callback(null, articleList);
         }
     });
-    // 输出结果
-    console.log(classList);
+}
+
+readArticleList('http://blog.sina.com.cn/s/articlelist_1776757314_0_1.html', function (err, articleList) {
+    if (err) console.error(err.stack);
+    console.log(articleList);
 });
