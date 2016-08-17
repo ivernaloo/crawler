0e99b677521dc982bec1f97cb3fd2b461652a83d
aloo <aloocs@gmail.com>
2016-08-17 17:15:22 +0800

    完成了翻页

--- web/js/index.js
+++ web/js/index.js
@@ -6,7 +6,24 @@ $(window).scroll(function(){
     if  ($(window).scrollTop() == $(document).height() - $(window).height()){
         // run our call for pagination
         console.log("count : ", COUNT);
+        loadList(COUNT);
         COUNT++;
     }
 
 });
+
+
+function loadList(pageNumber){
+    $.get(
+            "/page/" + pageNumber,
+            function(data){
+                console.log(" data : ", data.length);
+                if (data.length > 0){
+                    $(".list-group").append($(data));
+                } else {
+                    alert("到底了")
+                }
+
+            }
+        )
+}
