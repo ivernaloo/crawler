9ff39292937fd04379df72875559f30777f1af5f
aloo <aloocs@gmail.com>
2016-08-19 23:25:42 +0800

    更新定时器为12小时一次

--- config.js
+++ config.js
@@ -17,4 +17,4 @@ exports.db = mysql.createConnection({
 exports.port = 3000;
 
 // 定时更新
-exports.autoUpdate = '* * */12 * * *';  // 任务执行规则，参考 cron 语法
+exports.autoUpdate = '* */12 * * *';  // 任务执行规则，参考 cron 语法
