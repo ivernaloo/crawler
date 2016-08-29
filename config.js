aad92b338002d5d4c15bc077bf332b385f066fba
aloo <aloocs@gmail.com>
2016-08-29 09:32:22 +0800

    update example

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 // MySQL数据库连接配置
 var mysql = require('mysql');
-// exports.db = mysql.createConnection({
-//   host:            '114.215.139.174',   // 数据库地址
-//   port:            '3306',          // 数据库端口
-//   database:        'crawler',   // 数据库名称
-//   user:            'root',        // 数据库用户
-//   password:        'root'             // 数据库用户对应的密码
-// });
 exports.db = mysql.createConnection({
-  host:            'localhost',   // 数据库地址
+  host:            '114.215.139.174',   // 数据库地址
   port:            '3306',          // 数据库端口
   database:        'crawler',   // 数据库名称
-  user:            'user',        // 数据库用户
-  password:        'user'             // 数据库用户对应的密码
+  user:            'root',        // 数据库用户
+  password:        'root'             // 数据库用户对应的密码
 });
+// exports.db = mysql.createConnection({
+//   host:            'localhost',   // 数据库地址
+//   port:            '3306',          // 数据库端口
+//   database:        'crawler',   // 数据库名称
+//   user:            'user',        // 数据库用户
+//   password:        'user'             // 数据库用户对应的密码
+// });
 
 // 博客配置
 exports.res = [
