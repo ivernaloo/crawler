cd19f766f63063b22549370030c35096679cea4e
aloo <aloocs@gmail.com>
2016-09-01 18:51:01 +0800

    完成了抓取队列

--- crawler/app.js
+++ crawler/app.js
@@ -3,39 +3,39 @@ var cheerio = require('cheerio');
 var cloud = require('../data/leancloud');
 var async = require('async');
 
-var _URL = ["http://www.0daydown.com/category/tutorials/other"
-    , "http://www.0daydown.com/category/tutorials/移动app开发"
-    , "http://www.0daydown.com/category/tutorials/web-design"]
-
-// var COUNT = 0;
-// var SI = setInterval(function(){
-//     if ( COUNT < 1 ) {
-//         crawler(encodeURI(_URL[COUNT]));
-//         COUNT++
-//     } else {
-//         clearInterval(SI);
-//     }
-// }, 2000);
+var _URL = [
+        "http://www.0daydown.com/category/tutorials/other"
+      , "http://www.0daydown.com/category/tutorials/other/page/2"
+      , "http://www.0daydown.com/category/tutorials/other/page/3"
+      , "http://www.0daydown.com/category/tutorials/other/page/4"
+      , "http://www.0daydown.com/category/tutorials/other/page/5"
+      , "http://www.0daydown.com/category/tutorials/移动app开发"
+      , "http://www.0daydown.com/category/tutorials/移动app开发/page/2"
+      , "http://www.0daydown.com/category/tutorials/移动app开发/page/3"
+      , "http://www.0daydown.com/category/tutorials/移动app开发/page/4"
+      , "http://www.0daydown.com/category/tutorials/移动app开发/page/5"
+      , "http://www.0daydown.com/category/tutorials/web-design"
+      , "http://www.0daydown.com/category/tutorials/web-design/page/2"
+      , "http://www.0daydown.com/category/tutorials/web-design/page/3"
+      , "http://www.0daydown.com/category/tutorials/web-design/page/4"
+      , "http://www.0daydown.com/category/tutorials/web-design/page/5"
+];
 
 async.series([
     function(done){
-        async.eachSeries(Object.keys(_URL), function(index, urlnext){
-            crawler(encodeURI(_URL[index]), urlnext);
+        async.eachSeries(Object.keys(_URL), function(index, next){
+            crawler(encodeURI(_URL[index]), next);
         }, done);
     }
-
 ], function(err){
     if (err) console.error(err.stack);
 
     console.log('完成URL 列表遍历存储!');
-    // process.exit(0);
 });
 
-
-
-
-
-function crawler(url, callback){
+function crawler(url, callbackup){
+    var RESULTS = [];
+    console.log(" 开始 crawler 抓取 " + url);
     // request the target url
     request(url, function(error, response, body) {
         console.log("crawler 抓取 " + url);
@@ -64,70 +64,54 @@ function crawler(url, callback){
                 });
             });
 
-            callback(_list);
-        }
-    });
-}
+            async.series([
+                /*
+                * queue 1
+                * filter the exist item from the list
+                * */
+                function(done){
+                    var LIST = [];
 
-/*
-* 存储数据
-* */
-function record (){
-    // check record
-
-    // record list
-
-    // 检查存在与否，记录
-    async.series([
-        /*
-         * queue 1
-         * filter the exist item from the list
-         * */
-        function(done){
-            var LIST = [];
-            async.eachSeries(Object.keys(_list), function(index, asyncnext){
-
-                // todo: 修复检查与否的逻辑
-                cloud.checkExist(_list[index].body.url, function(flag){
-                    // true 删除多余的的元素
-                    if ( flag ) {
-                        console.log("重复的游标 :", index);
-                        LIST.push(index);
-                    }
-                    asyncnext();
-                })
-            }, function(){
-                console.log("LIST : ", LIST);
-                if ( LIST.length > 0 ){
-                    LIST.forEach(function(i, v){
-                        console.log(i, " - ", v);
+                    async.eachSeries(Object.keys(_list), function(index, next){
+                        cloud.checkExist(_list[index].body.url, function(flag){
+                            if ( flag ) {
+                                LIST.push(
+                                    parseInt(index)
+                                );
+                            }
+                            next();
+                        })
+                    }, function (){
+                        console.log("_list 删除重复之前 : ", _list.length);
+                        console.log("LIST : ", LIST);
+                        if ( LIST.length > 0 ){
+                            RESULTS =  _list.filter(function(item ,i){
+                                return LIST.indexOf(i) == -1;
+                            })
+                        }
+                        console.log("RESULTS 删除重复之后 : ", RESULTS.length);
+                        done();
                     })
+                },
+
+                /*
+                * queue 2
+                * store the list
+                * */
+                function(done){
+                    cloud.record({
+                        "requests" : RESULTS
+                    }, done);
+                    console.log('存储数量 : ', RESULTS.length);
                 }
-                done();
-            })
-        },
+            ], function(err){
+                if (err) console.error(err.stack);
 
-        /*
-         * queue 2
-         * store the list
-         * */
-        function(done){
-
-
-            // cloud.record({
-            //     "requests" : _list
-            // }, done);
-            // console.log('存储数量 : ', _list.length);
-            done();
+                console.log('完成队列');
+                callbackup && callbackup();
+                // process.exit(0);
+            });
         }
-    ], function(err){
-        if (err) console.error(err.stack);
-
-        console.log('完成队列');
-        callbackup && callbackup();
-        // process.exit(0);
     });
 }
-
-
 
